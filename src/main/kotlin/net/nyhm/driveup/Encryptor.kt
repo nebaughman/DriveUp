1affdab6916e21f72ea8d7712c779e5455ccbe9a
Nathaniel Baughman <neb@nebaughman.com>
2018-10-21 10:58:16 +0300

    Add explicit initialization process
    Instead of implicitly triggering a Google OAuth process if the user credential file is not found, this update provides an explicit 'init' command, which configures encryption, performs Google OAuth, and saves the data in a named 'creds' file.
    
    Warning: The creds file format is expected to change. The stored format is versioned, but versions will only be honored in tagged releases (not during development).

--- src/main/kotlin/net/nyhm/driveup/Encryptor.kt
+++ src/main/kotlin/net/nyhm/driveup/Encryptor.kt
@@ -1,14 +1,10 @@
 package net.nyhm.driveup
 
 import name.neuhalfen.projects.crypto.bouncycastle.openpgp.BouncyGPG
-import name.neuhalfen.projects.crypto.bouncycastle.openpgp.keys.keyrings.KeyringConfig
 import name.neuhalfen.projects.crypto.bouncycastle.openpgp.keys.keyrings.KeyringConfigs
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.util.io.Streams
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.InputStream
+import java.io.*
 import java.nio.file.Files
 import java.nio.file.Path
 import java.security.Security
@@ -26,30 +22,66 @@ object NonEncryptor: Encryptor {
   override fun remoteName(source: File) = source.name
 }
 
+class GpgConfig private constructor(
+    val publicKey: ByteArray,
+    val recipient: String
+) {
+
+  init {
+    initProvider()
+  }
+
+  internal val keyring = KeyringConfigs
+      .forGpgExportedKeys { _ -> null }
+      .also { it.addPublicKey(publicKey) }
+
+  fun export(): ByteArray {
+    val bytes = ByteArrayOutputStream()
+    DataOutputStream(bytes).use { out ->
+      out.writeInt(VERSION)
+      out.writeInt(publicKey.size)
+      out.write(publicKey)
+      out.writeInt(recipient.length) // char count (not byte count)
+      out.writeChars(recipient)
+    }
+    return bytes.toByteArray()
+  }
+
+  companion object {
+    private const val VERSION = 1
+
+    private fun initProvider() {
+      if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+        Security.addProvider(BouncyCastleProvider())
+      }
+    }
+
+    fun fromFile(publicKeyFile: Path, recipient: String): GpgConfig {
+      val publicKey = Files.readAllBytes(publicKeyFile)
+      return GpgConfig(publicKey, recipient)
+    }
+
+    fun fromBytes(bytes: ByteArray): GpgConfig {
+      DataInputStream(ByteArrayInputStream(bytes)).use { data ->
+        val ver = data.readInt()
+        if (ver != VERSION) throw IllegalArgumentException("Unexpected version $ver, expected $VERSION")
+        val publicKey = ByteArray(data.readInt())
+        data.readFully(publicKey)
+        val recipientLen = data.readInt() // char count (not byte count)
+        var recipient = ""
+        for (i in 0 until recipientLen) {
+          recipient += data.readChar()
+        }
+        return GpgConfig(publicKey, recipient)
+      }
+    }
+  }
+}
+
 /**
  * This class encrypts files to a specified [recipient], given the recipient's GPG/PGP [publicKey].
  */
-class GpgEncryptor(
-    private val publicKey: Path,
-    private val recipient: String
-): Encryptor {
-
-  private val keyringConfig: KeyringConfig
-
-  init {
-    if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
-      Security.addProvider(BouncyCastleProvider())
-    }
-
-    val publicKey = Files.readAllBytes(
-        //Paths.get(Encryptor::class.java.getResource(PUB_KEY_FILE).toURI())
-        publicKey
-    )
-
-    val keyring = KeyringConfigs.forGpgExportedKeys { _ -> null }
-    keyring.addPublicKey(publicKey)
-    keyringConfig = keyring
-  }
+class GpgEncryptor(private val config: GpgConfig): Encryptor {
 
   override fun remoteName(source: File) = source.name + GPG_FILE_EXT
 
@@ -57,19 +89,23 @@ class GpgEncryptor(
    * Encrypt the given [source] file.
    * This method produces an InputStream for consuming the encrypted content.
    */
-  override fun encrypt(source: File): InputStream {
+  override fun encrypt(source: File) = encrypt(source.inputStream())
+
+  // TODO: implement this class as a Stream?
+  //
+  fun encrypt(input: InputStream): InputStream {
     // TODO: use PipedInputStream and PipedOutputStream
     //val dest = File("${source.name}.gpg")
     //val result = FileOutputStream(dest)
     val result = ByteArrayOutputStream()
     BouncyGPG.encryptToStream()
-        .withConfig(keyringConfig)
+        .withConfig(config.keyring)
         .withStrongAlgorithms()
-        .toRecipient(recipient)
+        .toRecipient(config.recipient)
         .andDoNotSign()
         .binaryOutput()
         .andWriteTo(result).use { output ->
-          Streams.pipeAll(source.inputStream(), output)
+          Streams.pipeAll(input, output)
         }
     return ByteArrayInputStream(result.toByteArray())
   }
--- src/test/kotlin/net/nyhm/driveup/TestDriveUp.kt
+++ src/test/kotlin/net/nyhm/driveup/TestDriveUp.kt
@@ -7,4 +7,25 @@ class TestDriveUp {
   fun testMain() {
     main(emptyArray())
   }
+
+  @Test
+  fun testInit() {
+    main(arrayOf(
+        "init",
+        //"--help",
+        "--client-secret=local/creds/client_secret.json",
+        "--public-key=local/creds/public_key.asc",
+        "--encryption-recipient=test@example.com",
+        "--output=local/test.creds",
+        "--overwrite"
+    ))
+  }
+
+  @Test
+  fun testList() {
+    main(arrayOf(
+        "--config=local/test.creds",
+        "list"
+    ))
+  }
 }
